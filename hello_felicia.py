bb4085df50e24f75f973ce9e13a54e759d53b2ac
bhaamfe6 <68972882+bhaamfe6@users.noreply.github.com>
2022-12-13 00:15:00 -0600

    Delete hello_felicia.py

--- hello_felicia.py
+++
@@ -1,9 +0,0 @@
-print("Hello, Felicia!")
-print()
-respond = input("Is it morning or night? ")
-if respond == "morning":
-    print("Good morning!")
-else:
-    print("Go to sleep now!!!")
-print()
-pause = input("Please any key to continue")
