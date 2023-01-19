6eecbbe9095e91cb49c783bc1d51d7673f95f458
bhaamfe6 <68972882+bhaamfe6@users.noreply.github.com>
2023-01-18 22:12:11 -0600

    Add files via upload

---
+++ Programs/variables_casting_quizExample.py
@@ -0,0 +1,47 @@
+# input a float value for variable a here
+a = float(input("What is 3 + 4? "))
+while a != 7:
+    print("That is incorrect! Try again")
+    a = float(input("What is 3 + 4? "))
+    print(a)
+
+print("7 is correct!")
+print()
+    
+# input a float value for variable b here
+b = float(input("What is 15 - 5? "))
+while b != 10:
+    print("That is incorrect! Try again")
+    b = float(input("What is 15 - 5? "))
+    print(b)
+print("10 is correct!")
+print()
+# output the result of subtraction here
+print(b)
+
+
+#input a float value for variable c here
+c = float(input("What is 3 * 7? "))
+while c != 21:
+    print("That is incorrect! Try again")
+    c = float(input("What is 3 * 7? "))
+    print(c)
+print("21 is correct!")
+print()
+#input a float value for d variable here
+d = float(input("What is 25 divided by 5? "))
+
+while d != 5:
+    print("That is incorrect! Try again")
+    d = float(input("What is 25 / 5? "))
+    print(d)
+print("5 is correct!")
+print()
+print()
+print("Congratulations! You Have Answered all of the Questions!")
+print()
+print(input("Press Any Key To Continue!"))
+    
+print("\nThat's all, folks!")
+print()
+print(input("Press Any Key To Continue!"))
